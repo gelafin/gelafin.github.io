169abaff42661c014bbb602a4bd5be5657029c22
Mark <themarkymarkers@gmail.com>
2020-04-07 09:46:41 -0700

    rewrite test js

--- YayReplay/js/playDataUpload.js
+++ YayReplay/js/playDataUpload.js
@@ -1,4 +1,4 @@
-var target = document.querySelector('#play-data-container');
-var div = document.createElement('div');
-div.innerHTML = 'test text!';
-target.parentNode.insertBefore(div, target);
+var container = document.getElementById('play-data-container');
+var newDiv = document.createElement('div');
+newDiv.innerHTML = 'test text!';
+container.appendChild(newDiv);
