9c6f3c69ecf668a1ed88c0d9cdcd91d11c7aeb1c
Mark <themarkymarkers@gmail.com>
2020-04-07 11:13:46 -0700

    move script from head to body after relevant div

--- YayReplay/js/playDataUpload.js
+++
@@ -1,9 +0,0 @@
-
-function uploadPlayData(){
-  var container = document.getElementById('play-data-container');
-  var newDiv = document.createElement('div');
-  newDiv.innerHTML = 'test text!';
-  container.appendChild(newDiv);
-}
-
-document.onload = uploadPlayData();
