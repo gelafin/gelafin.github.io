08f09ad9c64decad436d209383ab59dd1f54d278
Mark <themarkymarkers@gmail.com>
2020-04-07 10:11:52 -0700

    delay upload function until onload

--- YayReplay/js/playDataUpload.js
+++ YayReplay/js/playDataUpload.js
@@ -1,4 +1,9 @@
-var container = document.getElementById('play-data-container');
-var newDiv = document.createElement('div');
-newDiv.innerHTML = 'test text!';
-container.appendChild(newDiv);
+
+function uploadPlayData(){
+  var container = document.getElementById('play-data-container');
+  var newDiv = document.createElement('div');
+  newDiv.innerHTML = 'test text!';
+  container.appendChild(newDiv);
+}
+
+document.onload = uploadPlayData();
