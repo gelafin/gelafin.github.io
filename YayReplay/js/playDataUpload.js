97efd0a3d2a1733f013e0f340ac2e557e9af8938
Mark <themarkymarkers@gmail.com>
2020-04-14 16:40:22 -0700

    create nested loop to add game data with classes

--- YayReplay/js/playDataUpload.js
+++ YayReplay/js/playDataUpload.js
@@ -3,10 +3,16 @@
 function uploadPlayData(parsedObject){
   var container = document.getElementById('play-data-container');
 
-  for (const row of parsedObject.data){
-    let newDiv = document.createElement('div');
-    newDiv.innerHTML = row;
-    container.appendChild(newDiv);
+  for (const gameRow of parsedObject.data){
+    let newRowDiv = document.createElement('div'); // create element with flex-flow: row nowrap using .className assignment
+    newRowDiv.className = 'flexbox-container game-row';
+    container.appendChild(newRowDiv); // new empty div to keep children in a row
+
+    for (column of parsedObject.meta.fields){
+      let newColumnDiv = document.createElement('div'); // row of children
+      newColumnDiv.innerHTML = gameRow[column];
+      newRowDiv.appendChild(newColumnDiv);
+    }
   }
 }
 
