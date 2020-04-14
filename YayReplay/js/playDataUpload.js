4abc0fc2cf43229f0666ed004c615f1eeae12d08
Mark <themarkymarkers@gmail.com>
2020-04-14 11:31:18 -0700

    make csv file path relative to index.html to resolve 404

--- YayReplay/js/playDataUpload.js
+++ YayReplay/js/playDataUpload.js
@@ -16,7 +16,7 @@ document.onload = uploadPlayData(iterations);
 /* test for Papaparse in console before adding to upload function */
 var data;
 
-Papa.parse('PlayAppData.csv', {
+Papa.parse('./js/PlayAppData.csv', {
   header: true,
   download: true,
   dynamicTyping: true,
