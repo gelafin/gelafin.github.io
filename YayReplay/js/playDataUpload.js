4d92a64cca8a214d153ab280794ec905d9442f25
Mark <themarkymarkers@gmail.com>
2020-04-21 17:44:33 -0700

    make uploadPlayData() a Manager method

--- YayReplay/js/playDataUpload.js
+++ YayReplay/js/playDataUpload.js
@@ -1,10 +1,5 @@
 /* papaparse imported using script tag */
 
-class Manager {
-  totalRowCounter = 1; //must start at 1 bc compares to array.length below
-  chunkRowsUploaded = 0; // keeps track of the 50 to avoid bug where like 40 results load and then oh no there's no more time to resume and resume calls upload50 from the beginning, which loads 50+40 from before = 90 results in one button press.
-}
-
 function createElementInside(newTag, containerId){
   var container = document.getElementById(containerId);
   let newElement = document.createElement(newTag); // create element with flex-flow: row nowrap using .className assignment
@@ -39,7 +34,7 @@ function getColumnId(column) {
     }
   }
 
-  return false; // TOOD: make this enum
+  return false; // TOOD: make this enum and consolidate with getColumnIdTemp
 }
 
 function getColumnIdTemp(column) {
@@ -76,74 +71,77 @@ function printHeaders(parsedObject){
   }
 }
 
-function uploadPlayData(parsedObject, parser){
-  for (const gameRow of parsedObject.data){
-    for (const column of parsedObject.meta.fields){
-      if (column === 'id') {
-        continue; // don't print id
-      } else if (column === 'image') {
-          let newImg = document.createElement('img');
-          newImg.src = gameRow[column];
-          newImg.className = 'game-data-item game-image';
-          document.getElementById('game-image-column').appendChild(newImg);
+class Manager {
+  totalRowCounter = 1; //must start at 1 bc compares to array.length below
+  chunkRowsUploaded = 0; // keeps track of the 50 to avoid bug where like 40 results load and then oh no there's no more time to resume and resume calls upload50 from the beginning, which loads 50+40 from before = 90 results in one button press.
 
-          // prepare hidden row underneath first item
-          var newRowDivHidden = document.createElement('div');
-          newRowDivHidden.className = 'flexbox-container game-details hidden';
-          newImg.appendChild(newRowDivHidden);
+  uploadPlayData(parsedObject, parser){
+    for (const gameRow of parsedObject.data){
+      for (const column of parsedObject.meta.fields){
+        if (column === 'id') {
+          continue; // don't print id
+        } else if (column === 'image') {
+            let newImg = document.createElement('img');
+            newImg.src = gameRow[column];
+            newImg.className = 'game-data-item game-image';
+            document.getElementById('game-image-column').appendChild(newImg);
 
-          continue; // short-circuit for efficiency
-      } else if (getColumnIdTemp(column) !== false) { // if column is non-hidden text-only. TODO: how to save returned value to avoid a second call?
-          let newColumnDiv = document.createElement('div');
-          newColumnDiv.innerHTML = gameRow[column];
-          newColumnDiv.className = 'game-data-item';
-          document.getElementById(getColumnId(column)).appendChild(newColumnDiv);
-          continue;
-      } else { // it's a hidden details item
-        // TODO: Once search is ready, make hidden items load in a separate function called by button
-        // each game has a hidden details div that never interacts with layout. The button that shows this will also create space for it
-          if (column === 'video') {
-            if (gameRow[column] != 'none') {
-              let newIframe = document.createElement('iframe');
-              newIframe.src = gameRow[column];
-              newIframe.className = 'game-data-item';
-              newRowDivHidden.appendChild(newIframe);
+            // prepare hidden row underneath first item
+            var newRowDivHidden = document.createElement('div');
+            newRowDivHidden.className = 'flexbox-container game-details hidden';
+            newImg.appendChild(newRowDivHidden);
+
+            continue; // short-circuit for efficiency
+        } else if (getColumnIdTemp(column) !== false) { // if column is non-hidden text-only. TODO: how to save returned value to avoid a second call?
+            let newColumnDiv = document.createElement('div');
+            newColumnDiv.innerHTML = gameRow[column];
+            newColumnDiv.className = 'game-data-item';
+            document.getElementById(getColumnId(column)).appendChild(newColumnDiv);
+            continue;
+        } else { // it's a hidden details item
+          // TODO: Once search is ready, make hidden items load in a separate function called by button
+          // each game has a hidden details div that never interacts with layout. The button that shows this will also create space for it
+            if (column === 'video') {
+              if (gameRow[column] != 'none') {
+                let newIframe = document.createElement('iframe');
+                newIframe.src = gameRow[column];
+                newIframe.className = 'game-data-item';
+                newRowDivHidden.appendChild(newIframe);
+              }
+              continue;
+            } else if (column === 'previewImageList') {
+              // makes new img element for each. uris are separated by comma
+              previewImages = gameRow[column].split(', ');
+              for (const uri of previewImages) {
+                let newImgTag = document.createElement('img');
+                newImgTag.src = uri;
+                newImgTag.className = 'game-data-item';
+                newRowDivHidden.appendChild(newImgTag);
+              }
+              continue;
+            } else if (column === 'link') {
+              let newAnchor = document.createElement('a');
+              newAnchor.innerHTML = 'Google Play page';
+              newAnchor.title = 'Google Play page';
+              newAnchor.href = gameRow[column];
+              newAnchor.target = "_blank";
+              newAnchor.className = 'game-data-item';
+              newRowDivHidden.appendChild(newAnchor);
+              continue;
+            } else { // text-only hidden details item
+              let newDiv = document.createElement('div');
+              newDiv.innerHTML = gameRow[column];
+              newRowDivHidden.appendChild(newDiv);
             }
-            continue;
-          } else if (column === 'previewImageList') {
-            // makes new img element for each. uris are separated by comma
-            previewImages = gameRow[column].split(', ');
-            for (const uri of previewImages) {
-              let newImgTag = document.createElement('img');
-              newImgTag.src = uri;
-              newImgTag.className = 'game-data-item';
-              newRowDivHidden.appendChild(newImgTag);
-            }
-            continue;
-          } else if (column === 'link') {
-            let newAnchor = document.createElement('a');
-            newAnchor.innerHTML = 'Google Play page';
-            newAnchor.title = 'Google Play page';
-            newAnchor.href = gameRow[column];
-            newAnchor.target = "_blank";
-            newAnchor.className = 'game-data-item';
-            newRowDivHidden.appendChild(newAnchor);
-            continue;
-          } else { // text-only hidden details item
-            let newDiv = document.createElement('div');
-            newDiv.innerHTML = gameRow[column];
-            newRowDivHidden.appendChild(newDiv);
-          }
+        }
       }
+      //after for loop goes through one game, pause parsing
+      parser.pause();
     }
-    //after for loop goes through one game, pause parsing
-    parser.pause();
   }
 }
 
 manager = new Manager;
-alert(manager.totalRowCounter);
-alert(manager.chunkRowsUploaded);
 
 Papa.RemoteChunkSize = 100000; // set chunk to less than the file for testing
 
@@ -152,5 +150,5 @@ document.onload = Papa.parse('./assets/PlayAppData.csv', {
                     download: true,
                     dynamicTyping: true,
                     skipEmptyLines: true,
-                    chunk: uploadPlayData
+                    chunk: manager.uploadPlayData
                   });
